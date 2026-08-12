66bb524b92dca3f383cb457fee6881e5e514f31d
Abhimanyu02504 <abhimanyu02504@gmail.com>
2026-08-12 09:05:46 +0530

    Day01

---
+++ 03JAVASCRIPT/Day 01/first.js
@@ -0,0 +1,22 @@
+// for print the statement we use console.log
+
+console.log("Hello Coder Army");
+
+// if we have to declare any variable character strng we use "let"  also in the let we dont have to telll that what i want to declare just write let and it will do all these thing by owns
+// in the let option when we use let then after i will chnage it but in other we cant change it
+let abhi=10;
+abhi=20;
+console.log(abhi);
+
+
+// for taking value which we cant change if we define once then use "const"
+
+const abhimanyu =98;
+console.log(abhimanyu)
+
+
+// for taking input or say to declare thing one method also which is oldeer due to some 
+// disadvantage we cant usse this "var"  and also in the var we change the value of pre defined value  
+var abhim=97;
+abhim=99;
+console.log(abhim);
