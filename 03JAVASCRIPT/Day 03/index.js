9e9a42ce6ea1ed4bc2c14a9ba5a9511e2cc3de75
Abhimanyu02504 <abhimanyu02504@gmail.com>
2026-08-13 08:26:40 +0530

    O

---
+++ 03JAVASCRIPT/Day 03/index.js
@@ -0,0 +1,4 @@
+// in the operator null == undefined is always true
+// but null === undefined it is always false === equal to means it check first type then it compare
+console.log(null == undefined);
+console.log(null===undefined);
