ee03b90c0dfbb29018f239ac73bd7bd7d348ff63
Abhimanyu02504 <abhimanyu02504@gmail.com>
2026-08-13 08:24:17 +0530

    operator

--- 03JAVASCRIPT/Day 02/first.js
+++ 03JAVASCRIPT/Day 02/first.js
@@ -1,2 +1,40 @@
-let arr= [10,20,30,"Mohit","Abhimanyu"]
-console.log(arr)
+// let arr= [10,20,30,"Mohit","Abhimanyu"]
+// console.log(arr)
+// console.log(typeof arr)
+
+// object always make in key value pair 
+
+// let obj = {
+//     user_name : "Raj",
+//     Account_num : 123456789,
+//     Balance : 99999999999999,
+//     Village : "Umran"
+// }
+// console.log(obj);
+
+
+// when we want to convert any other type to any other type  NaN means Not a number
+
+let x = false;
+console.log(Number(x));
+
+let abhi= "100xs";
+console.log(Number(abhi))
+let x1= null;
+console.log(Number(x1));
+
+let abhim = 20;
+console.log(String(abhim));
+
+
+// in this case where 
+
+console.log(6*3+18/6-9)
+// Divide Multiply left to right  
+// Add subtract left to right
+// MOdulus gives reminder
+
+// in the operator null == undefined is always true
+// but null === undefined it is always false === equal to means it check first type then it compare
+console.log(null == undefined);
+console.log(null===undefined);
