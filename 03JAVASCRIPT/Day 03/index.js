cd13603ca03063095d4dc7f131a725532cea3ad8
Abhimanyu02504 <abhimanyu02504@gmail.com>
2026-08-13 17:49:34 +0530

    O\

--- 03JAVASCRIPT/Day 03/index.js
+++ 03JAVASCRIPT/Day 03/index.js
@@ -1,4 +1,35 @@
 // in the operator null == undefined is always true
 // but null === undefined it is always false === equal to means it check first type then it compare
-console.log(null == undefined);
-console.log(null===undefined);
+// console.log(null == undefined);
+// console.log(null===undefined);
+// console.log(null ==0);
+// console.log(null <=0);
+// console.log(null >=0);
+// console.log(null>0);
+// console.log(null<0);
+
+
+// because undefined is Not a Number
+// console.log(undefined ==0);
+// console.log(undefined <=0);
+// console.log(undefined >=0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(NaN == NaN);
+
+
+// bitwise operators
+
+// it should add like binary operators means first convert binary of 4 and then binary of 5 then after add 100 and 101
+// then it gives 0and 1 = 0 then 1 and 1 = 1 then answer will be  1 0 0 and it is the binary of 4 
+// isi trh sare bitwise operator work krte hi "and","or"
+console.log(4&5);
+console.log(11|14);
+// ^ it is the sign of EXOR gate 
+console.log(2^5);
+console.log(5<<3);
+// << it means 5 multiply by 2 power 3 
+// 101.000000 it means shift this decimal 3 in right then answer will be 101000..000  and it is also called left shift 
+console.log(20>>2);
+
+// 10100.00 it is shift 2 in right then it will be 101 then its answer will be 5
