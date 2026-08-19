461050ab4eac761e3bc269bb1255918383737f87
Abhimanyu02504 <abhimanyu02504@gmail.com>
2026-08-19 18:16:43 +0530

    catenation

--- 03JAVASCRIPT/Day 04/index.js
+++ 03JAVASCRIPT/Day 04/index.js
@@ -6,10 +6,29 @@
 // Non-Primitive Data type
 // But in non primitive we can change the value
 
-const obj={
-    id : 20,
-    balance : 2525525
+// const obj={
+//     id : 20,
+//     balance : 2525525
 
-}
-obj.id = 25;
-console.log(obj)
+// }
+// obj.id = 25;
+// console.log(obj)
+
+// String in JS
+
+// let str1 = "Hello coder Army";
+// let str2 = `Hello coder Army`;
+// let str3 = 'Hello coder Army';
+// console.log(str1,str2,str3);
+
+
+// Display the value use `` this ${} and then this 
+
+// let price = 50;
+// console.log(`price of this car is ${price}`);
+// console.log("Price of this car is ",price,"get as soonn aasap");
+
+// " Hello Coder Army" print this
+// console.log(`" Hello Coder Army"`)
+
+
