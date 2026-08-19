071601a0ff8681d72e03c301c506f183c760c549
Abhimanyu02504 <abhimanyu02504@gmail.com>
2026-08-19 18:02:27 +0530

    O

---
+++ 03JAVASCRIPT/Day 04/index.js
@@ -0,0 +1,15 @@
+// Primitive Datatype
+// const num=10;
+// num =20;  //it is not chnaged and its give error becuase its an constant value and we cant change in constant value.
+// console.log(num);
+
+// Non-Primitive Data type
+// But in non primitive we can change the value
+
+const obj={
+    id : 20,
+    balance : 2525525
+
+}
+obj.id = 25;
+console.log(obj)
