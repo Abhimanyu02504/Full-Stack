f968468b1aefd614ae5ee1591551f07a4f3e84f0
Abhimanyu02504 <abhimanyu02504@gmail.com>
2026-08-22 10:55:02 +0530

    Update Day 07

--- 03JAVASCRIPT/Day 06/index.js
+++ 03JAVASCRIPT/Day 06/index.js
@@ -1,19 +1,35 @@
-const arr = [1,25,446,"Rohit","Abhi"];
-console.log(arr);
-console.log(arr.length); // it give answer 5 because length of this array is 5
-console.log(arr.at(-2)); // this is use to find word 
-// if we want to insert new value in array in the last 
- arr.push(50);
-//  pop is used to remove the last word of our array 
- arr.pop();
-// if we have to unsert valur in begining 
-// unshift is used to add element in starting
-arr.unshift(10);
-// for delete from starting use shift
+const arr = [1,25,446,"Rohit","Abhi",56,78,46,451];
+// console.log(arr);
+// console.log(arr.length); // it give answer 5 because length of this array is 5
+// console.log(arr.at(-2)); // this is use to find word 
+// // if we want to insert new value in array in the last 
+//  arr.push(50);
+// //  pop is used to remove the last word of our array 
+//  arr.pop();
+// // if we have to unsert valur in begining 
+// // unshift is used to add element in starting
+// arr.unshift(10);
+// // for delete from starting use shift
 
-arr.shift();
-// when you want to delet any particular value of indexing then use delete but ye particular delete nhi krta hi delete krta hi but uska space rhta hi 
-// it will give the answer like this  Rohit  [ 1, 25, <1 empty item>, 'Rohit', 'Abhi' ]
-delete arr[2];
+// arr.shift();
+// // when you want to delet any particular value of indexing then use delete but ye particular delete nhi krta hi delete krta hi but uska space rhta hi 
+// // it will give the answer like this  Rohit  [ 1, 25, <1 empty item>, 'Rohit', 'Abhi' ]
+// delete arr[2];
 
-console.log(arr);
+// console.log(arr);
+// console.log(arr.indexOf(25));  // its use for to find index of number letter or anything which write in the place of (_)
+// console.log(arr.lastIndexOf()); //when we use lastindexof when same element present two or three time rhen we use this 
+// console.log(arr.includes(25)); // we use this because we have to find that this element is in the array or not
+// console.log(arr.slice(2,4)); //its means cut the array betweeen these two element
+// console.log(arr.splice(2,4)); // its means ki index 2 se aage ki 4 array ise de do and splice aalso change you main array 
+
+
+let arr1=[1,2,3,4,5];
+let arr2 = [6,7,8,9];
+let arr3 = arr1.concat(arr2);
+console.log(arr3);
+
+// baki sbkuchh padhaya tha 2d array ke baare me 
+// float fnction se hm array ko change kr skte hi 1 2 3 4 kisi bhi form me 
+
+
